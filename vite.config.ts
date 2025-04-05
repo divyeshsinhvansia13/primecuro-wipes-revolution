44a2a8dc1ad0ac47ff1533b4a5a73c866af73486
divyeshsinhvansia13 <143331671+divyeshsinhvansia13@users.noreply.github.com>
2025-04-05 22:51:16 +0000

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path"; // Import Node's path module
 
 export default defineConfig({
-  base: "./", // Use a relative base so assets load correctly on GitHub Pages
+  base: "/", // Use a relative base so assets load correctly on GitHub Pages
   server: {
     host: "0.0.0.0",
     port: 8080,
